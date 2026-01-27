fb2b480aa1597b7bd409ce02e1f1cd367ffa26e6
yhr <danieldovalle10@gmail.com>
2026-01-27 02:19:52 -0300

    Input Actions and Setup update

--- Runtime/Input/PlayerInputReader.cs
+++ Runtime/Input/PlayerInputReader.cs
@@ -3,68 +3,71 @@ using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using static PlayerInputActions;
 
-[CreateAssetMenu(fileName = "PlayerInputReader", menuName = "Scriptable Objects/PlayerInputReader")]
-public class PlayerInputReader : ScriptableObject, IPlayerInputReader, IPlayerActions
+namespace PersonalPackage.Input
 {
-    public event UnityAction<Vector2> Move = delegate { };
-    public event UnityAction<bool> Jump = delegate { };
-
-    private PlayerInputActions inputActions;
-
-    public Vector2 Direction => inputActions.Player.Move.ReadValue<Vector2>();
-    public bool IsJumpPressed => inputActions.Player.Jump.IsPressed();
-
-    public void EnablePlayerInputActions()
+    [CreateAssetMenu(fileName = "PlayerInputReader", menuName = "Scriptable Objects/PlayerInputReader")]
+    public class PlayerInputReader : ScriptableObject, IPlayerInputReader, IPlayerActions
     {
-        if (inputActions == null)
+        public event UnityAction<Vector2> Move = delegate { };
+        public event UnityAction<bool> Jump = delegate { };
+
+        private PlayerInputActions inputActions;
+
+        public Vector2 Direction => inputActions.Player.Move.ReadValue<Vector2>();
+        public bool IsJumpPressed => inputActions.Player.Jump.IsPressed();
+
+        public void EnablePlayerInputActions()
         {
-            inputActions = new PlayerInputActions();
-            inputActions.Player.SetCallbacks(this);
+            if (inputActions == null)
+            {
+                inputActions = new PlayerInputActions();
+                inputActions.Player.SetCallbacks(this);
+            }
+
+            inputActions.Enable();
         }
 
-        inputActions.Enable();
-    }
-
-    public void DisablePlayerInputActions()
-    {
-        inputActions?.Disable();
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        Move.Invoke(context.ReadValue<Vector2>());
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        switch (context.phase)
+        public void DisablePlayerInputActions()
         {
-            case InputActionPhase.Started:
-                Jump.Invoke(true);
-                break;
-            case InputActionPhase.Canceled:
-                Jump.Invoke(false);
-                break;
+            inputActions?.Disable();
         }
-    }
 
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        // nope
-    }
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Move.Invoke(context.ReadValue<Vector2>());
+        }
 
-    public void OnFire(InputAction.CallbackContext context)
-    {
-        // nope
-    }
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    Jump.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    Jump.Invoke(false);
+                    break;
+            }
+        }
 
-    public void OnMouseControlCamera(InputAction.CallbackContext context)
-    {
-        // nope
-    }
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            // nope
+        }
 
-    public void OnRun(InputAction.CallbackContext context)
-    {
-        // nope
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            // nope
+        }
+
+        public void OnMouseControlCamera(InputAction.CallbackContext context)
+        {
+            // nope
+        }
+
+        public void OnRun(InputAction.CallbackContext context)
+        {
+            // nope
+        }
     }
 }
